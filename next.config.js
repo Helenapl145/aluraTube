d4b61ea28ed83f1bc838a8a4adcb6426cc417a00
Helena <helenapl145@gmail.com>
2022-12-07 10:03:31 -0300

    todos os arquivos

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import { 'next' }.NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    complier: {
+        styledComponents: true,
+    },
+}
+
+module.exports = nextConfig
